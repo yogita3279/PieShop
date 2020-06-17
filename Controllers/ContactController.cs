8972333416c6efaf68cbc6f6ef686877666d32d1
yogita <yogita3279@hotmail.com>
2020-06-16 22:03:20 -0700

    added contact page with custom email tag helper and categories dropdown.

---
+++ Controllers/ContactController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PieShop.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- Controllers/PieController.cs
+++ Controllers/PieController.cs
@@ -26,13 +26,38 @@ namespace PieShop.Controllers
         }
         // GET: api/<PieApiController>
         [HttpGet]
-        public ViewResult List()
+        //public ViewResult List()
+        //{
+        //    PiesListViewModel piesListViewModel = new PiesListViewModel();
+        //    piesListViewModel.Pies = _pieRepository.AllPies;
+        //    piesListViewModel.CurrentCategory = "Cheese cakes";
+        //    //to get the html view in response 
+        //    return View(piesListViewModel);
+        //}
+
+
+        public ViewResult List(string category)
         {
-            PiesListViewModel piesListViewModel = new PiesListViewModel();
-            piesListViewModel.Pies = _pieRepository.AllPies;
-            piesListViewModel.CurrentCategory = "Cheese cakes";
-            //to get the html view in response 
-            return View(piesListViewModel);
+            IEnumerable<Pie> pies;
+            string currentCategory;
+
+            if (string.IsNullOrEmpty(category))
+            {
+                pies = _pieRepository.AllPies.OrderBy(p => p.PieId);
+                currentCategory = "All pies";
+            }
+            else
+            {
+                pies = _pieRepository.AllPies.Where(p => p.Category.CategoryName == category)
+                    .OrderBy(p => p.PieId);
+                currentCategory = _categoryRepository.AllCategories.FirstOrDefault(c => c.CategoryName == category)?.CategoryName;
+            }
+
+            return View(new PiesListViewModel
+            {
+                Pies = pies,
+                CurrentCategory = currentCategory
+            });
         }
 
         // GET api/<PieApiController>/5
